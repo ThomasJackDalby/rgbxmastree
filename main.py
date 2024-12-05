968ba3f53bb7c2d55fdc6013860135c8954fb9c1
Tom Dalby <thomasjackdalby@gmail.com>
2024-12-05 14:07:34 +0000

    Missed a bit off the extension

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from tree import RGBXmasTree
 PATTERNS = {}
 sys.path.insert(0, PATTERNS_FOLDER_PATH)
 for file_name in os.listdir(PATTERNS_FOLDER_PATH):
-    file_name_wo_ext = os.path.splitext(file_name)
+    file_name_wo_ext = os.path.splitext(file_name)[0]
     pattern = importlib.import_module(file_name_wo_ext)
     PATTERNS[file_name_wo_ext] = pattern
 
