272241f6d2022b0eb3cc8fae7393fd7b1dc79648
Tom Dalby <thomasjackdalby@gmail.com>
2024-12-05 13:51:59 +0000

    Adding test code

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
+import os
+import sys
 import asyncio
+import random
 
+sys.path.append(os.path.join(os.path.dirname(__file__), "rgbxmastree"))
+
+from tree import RGBXmasTree
+
+tree = RGBXmasTree()
+
+def random_color():
+    r = random.random()
+    g = random.random()
+    b = random.random()
+    return (r, g, b)
+
+try:
+    while True:
+        pixel = random.choice(tree)
+        pixel.color = random_color()
+except KeyboardInterrupt:
+    tree.close()
