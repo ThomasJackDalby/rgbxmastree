8715df80b73a33831b40904baa1c8abf4a2a6ed6
Tom Dalby <thomasjackdalby@gmail.com>
2024-12-15 19:46:07 +0000

    index error

--- patterns/pixels.py
+++ patterns/pixels.py
@@ -6,5 +6,5 @@ def run(tree):
             tree[i].color = (255, 0, 0)
         else:
             tree[i].color = (255, 255, 255)
-        tree[(i-1) % 25] = (0, 0, 0)
+        tree[(i-1) % 25].color = (0, 0, 0)
         sleep(0.5)
