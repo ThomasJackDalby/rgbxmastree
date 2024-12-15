00fbb1e9a67ce352589e1b92dfdd544428e69706
Tom Dalby <thomasjackdalby@gmail.com>
2024-12-15 20:06:03 +0000

    slight messup

--- patterns/pixels.py
+++ patterns/pixels.py
@@ -2,10 +2,11 @@ from time import sleep
 from tools import *
 
 def run(tree):
-    for i in BY_Z[0]:
-        if i == 0:
-            tree[i].color = (255, 0, 0)
+    sequence = BY_Z[0]
+    for i, id in enumerate(sequence):
+        if id == 0:
+            tree[id].color = (255, 0, 0)
         else:
-            tree[i].color = (255, 255, 255)
-        tree[(i-1) % 25].color = (0, 0, 0)
+            tree[id].color = (255, 255, 255)
+        tree[sequence[i-1]].color = (0, 0, 0)
         sleep(0.25)
