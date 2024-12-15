f7dea9b18fa3be72b698ae14d7cc37242508b28e
Tom Dalby <thomasjackdalby@gmail.com>
2024-12-15 20:18:39 +0000

    new sequence

--- patterns/pixels.py
+++ patterns/pixels.py
@@ -1,7 +1,7 @@
 from time import sleep
 from tools import *
 
-def run(tree):
+def run2(tree):
     sequence = BY_Z[0]
     for i, id in enumerate(sequence):
         if id == 0:
@@ -9,3 +9,20 @@ def run(tree):
         else:
             tree[id].color = (255, 255, 255)
         tree[sequence[i-1]].color = (0, 0, 0)
+
+def run(tree):
+
+    colors = [
+        (255, 0, 0),
+        (0, 255, 0),
+        (0, 0, 255),
+        (255, 0, 255),
+        (255, 255, 0),
+        (0, 255, 255),
+    ]
+    for z in range(4):
+        color = colors[z]
+        for i in range(len(BY_Z[0])):
+            tree[BY_Z[i]].color = color
+    
+    
--- tools.py
+++ tools.py
@@ -1,14 +1,10 @@
-
 # need a funky 3d reference for the lights on the tree
 
-
-
-BY_FACE = {}
-BY_SIDE = {}
-BY_COORD = {
-    (0, 0, 0)
-}
-
 BY_Z = {
-    0 : [0, 7, 19, 24, 12, 6, 15, 16]
+    0 : [0, 7, 19, 24, 12, 6, 15, 16],
+    1 : [1, 8, 20, 23, 11, 5, 14, 17],
+    2 : [2, 9, 21, 22, 10, 4, 13, 18],
+    3 : [3]
 }
+BY_FACE = [[BY_Z[0][i], BY_Z[1][i], BY_Z[2][i]] for i in range(8)]
+BY_SIDE = [BY_FACE[2*i] + BY_FACE[2*i + 1] for i in range(4)]
