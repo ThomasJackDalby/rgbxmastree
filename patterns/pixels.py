95612be0734c09f4b0f1e94ab6c1644036677c31
Tom Dalby <thomasjackdalby@gmail.com>
2024-12-15 20:03:36 +0000

    testing bottom layer

--- patterns/pixels.py
+++ patterns/pixels.py
@@ -1,10 +1,11 @@
 from time import sleep
+from tools import *
 
 def run(tree):
-    for i in range(25):
+    for i in BY_Z[0]:
         if i == 0:
             tree[i].color = (255, 0, 0)
         else:
             tree[i].color = (255, 255, 255)
         tree[(i-1) % 25].color = (0, 0, 0)
-        sleep(0.5)
+        sleep(0.25)
---
+++ tools.py
@@ -0,0 +1,14 @@
+
+# need a funky 3d reference for the lights on the tree
+
+
+
+BY_FACE = {}
+BY_SIDE = {}
+BY_COORD = {
+    (0, 0, 0)
+}
+
+BY_Z = {
+    0 : [0, 7, 19, 24, 12, 6, 15, 16]
+}
